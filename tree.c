406816a07a624c30799953839ecd5f3613494dd5
Kelley Kelley <mike1555@colorado.edu>
2021-11-30 12:15:15 -0700

    bezeir trees baby
    yea, they uh look weird right now but I'm one step closer

--- tree.c
+++ tree.c
@@ -13,6 +13,35 @@ struct point controls[50];
 int parents[50];
 // the first empty position
 int n = 0;
+// leaf nodes
+int leaves[50];
+int nleaves = 0;
+
+typedef struct { double x, y, z; } Point;
+
+//  Interpolation matrix for Bezier
+static double Mi[4][4] =
+{
+   { 1.00000000, 0.00000000, 0.00000000, 0.00000000},
+   {-0.83333333, 3.00000000,-1.50000000, 0.33333333},
+   { 0.33333333,-1.50000000, 3.00000000,-0.83333333},
+   { 0.00000000, 0.00000000, 0.00000000, 1.00000000},
+};
+
+// This routine multiplies a 4 x 4 matrix with a vector of 4 points.
+void Pmult(double M[4][4], Point v[4], Point r[4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		r[i].x = r[i].y = r[i].z = 0;
+		for (int k = 0; k < 4; k++)
+		{
+			r[i].x += M[i][k] * v[k].x;
+			r[i].y += M[i][k] * v[k].y;
+			r[i].z += M[i][k] * v[k].z;
+		}
+	}
+}
 
 // draw a tree
 // I could've just copied from my previous hw, but 
@@ -37,21 +66,83 @@ void tree(int x, int y, int z) {
 	controls[1] = top;
 	parents[1] = 0;
 	n = 2;
+	nleaves = 0;
 
 	// recursive functions that will populate our other branches
 	branch(1, 5);
 	branch(1, 5);
 
 	// set n to how many points we have
-	n -= 1;
+	//n -= 1;
 	
 	// draw the centers of our tree
+	/*
 	glBegin(GL_LINES);
 	for (int i = n; i >= 0; i--) {
 		glVertex3f(controls[i].x, controls[i].y, controls[i].z);
 		glVertex3f(controls[parents[i]].x, controls[parents[i]].y, controls[parents[i]].z);
 	}
 	glEnd();
+	*/
+	//pn -= 1;
+	/*
+	glBegin(GL_LINE_STRIP);
+	for (int i = 0; i < pn; i++) {
+		glVertex3f(P[i].x, P[i].y, P[i].z);
+	}
+	glEnd();
+	*/
+	
+	/*
+	for (int j = 0; j < nleaves; j++) {
+		//printf("%i=%i\n", j, leaves[j]);
+		
+		glBegin(GL_LINE_STRIP);
+		for (int i = leaves[j]; i > 0; i = parents[i]) {
+			glVertex3f(controls[i].x, controls[i].y, controls[i].z);
+		}
+		glVertex3f(controls[0].x, controls[0].y, controls[0].z);
+		glEnd();
+		
+	}
+	*/
+
+	/*
+	glBegin(GL_POINTS);
+	for (int j = 0; j < n; j++) {
+		glVertex3f(controls[j].x, controls[j].y, controls[j].z);
+		printf("point={%f,%f,%f}, parent=%i\n", controls[j].x, controls[j].y, controls[j].z, parents[j]);
+	}
+	glEnd();
+	*/
+
+	glEnable(GL_MAP1_VERTEX_3);
+
+	for (int j = 0; j < nleaves; j++) {
+		Point P[4];
+		int pn = 0;
+		for (int i = leaves[j]; i > 0; i = parents[i]) {
+			//printf("%i\n", i);
+			P[pn].x = controls[i].x;
+			P[pn].y = controls[i].y;
+			P[pn].z = controls[i].z;
+			pn += 1;
+		}
+		P[pn].x = controls[0].x;
+		P[pn].y = controls[0].y;
+		P[pn].z = controls[0].z;
+		pn += 1;
+
+		int m = 10;
+		Point r[4];
+		//  Transform 4 data points to 4 control points
+		Pmult(Mi, P, r);
+		//  Draw curve
+		glColor3f(1, 1, 0);
+		glMap1d(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, (void*)r);
+		glMapGrid1f(m, 0.0, 1.0);
+		glEvalMesh1(GL_LINE, 0, m);
+	}
 
 	// we are done put it back
 	glPopMatrix();
@@ -60,8 +151,7 @@ void tree(int x, int y, int z) {
 void branch(int parent, int iterations) {
 	// decrease iterations by 1
 	iterations -= 1;
-	if (iterations <= 1)
-		return;
+		
 	// add some random stuff to our base
 	struct point tmppoint = controls[parent];
 	float negx = Lehmer32() % 2 == 0 ? 1.0 : -1.0;
@@ -72,9 +162,16 @@ void branch(int parent, int iterations) {
 	
 	controls[n] = tmppoint;
 	parents[n] = parent;
+	int thisparent = n;
 	n += 1;
 
+	if (iterations - 1 <= 2) {
+		leaves[nleaves] = n-1;
+		nleaves += 1;
+		return;
+	}
+
 	// call the next recursive stuff
-	branch(parent+1, iterations);
-	branch(parent+1, iterations);
+	branch(thisparent, iterations);
+	branch(thisparent, iterations);
 }
