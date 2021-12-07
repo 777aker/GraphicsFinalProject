cfaf2e548753cb1014b96e57f45752f0d6217b0f
Kelley Kelley <mike1555@colorado.edu>
2021-12-07 01:14:20 -0700

    trying to add audio

---
+++ audio.c
@@ -0,0 +1,4 @@
+#include "audio.h"
+
+// how to do audio @
+// https://lazyfoo.net/tutorials/SDL/21_sound_effects_and_music/index.php
---
+++ audio.h
@@ -0,0 +1,10 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <string>
+
+#endif // !AUDIO_H
