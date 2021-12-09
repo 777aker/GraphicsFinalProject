83ce82e38f59081dd4f295725feee0ca7a03ce7a
Kelley Kelley <mike1555@colorado.edu>
2021-12-09 11:43:01 -0700

    final submission

--- displayfunc.h
+++ displayfunc.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef DISPLAYFUNC_H
 #define DISPLAYFUNC_H
 
@@ -30,36 +29,3 @@ extern int gensize;
 extern float playerpos[3];
 
 #endif
-=======
-#ifndef DISPLAYFUNC_H
-#define DISPLAYFUNC_H
-
-#include "glututility.h"
-#include "perlin.h"
-#include "tree.h"
-#include "audio.h"
-#include "particleengine.h"
-
-// --- functions ---
-// do this when nothing else to do
-void idle();
-// resize window
-void reshape(int width, int height);
-// do this when a key pressed
-void key(unsigned char ch, int x, int y);
-// do this when a special key is pressed
-void special(int key, int x, int y);
-// do this a lot
-void display();
-// initialize debug stuff
-void initdebug();
-// do the lighting stuff
-void light();
-
-// --- variables ---
-//extern type name; then define in c
-extern int gensize;
-extern float playerpos[3];
-
-#endif
->>>>>>> a501c129d71414e4ada3d249c7311bb5406b1ca7
